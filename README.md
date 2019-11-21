c15a29f395945856b6a570d3c6c1c293f568294b
LMR589281 <lukeryan1@virginmedia.com>
2019-11-21 18:06:31 +0000

    finished 4 and 5, added comments

--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 Base repository for COMP110 worksheet 6
  
 2-d array example (https://docs.microsoft.com/en-us/dotnet/csharp/programming-guide/arrays/multidimensional-arrays)
+for loops (https://www.tutorialsteacher.com/csharp/csharp-for-loop)
--- comp110-worksheet-6/OxoBoard.cs
+++ comp110-worksheet-6/OxoBoard.cs
@@ -14,8 +14,6 @@ namespace comp110_worksheet_6
         // Uncomment the optional parameters if attempting the stretch goal -- keep the default values to avoid breaking unit tests.
         public OxoBoard(/* int width = 3, int height = 3, int inARow = 3 */) {
             public int[,] board = new int[3, 3]; //declare list as a 2d array 
-            public int pointerX = 0; //declare x-axis pointer
-            public int pointerY = 0; //declare y-axis pointer
             //return board;
     }
 }
@@ -25,18 +23,18 @@ namespace comp110_worksheet_6
 
 		// Return the contents of the specified square.
 		public Mark GetSquare(int x, int y){
-            if (board(x, y) == Mark.None)
+            if (board(x, y) == Mark.None)//checks if empty 
             {
-                return Mark.None;
-            }
-            else if (board(x, y) == Mark.O)
-            {
-                return Mark.O;
-            }
-            else if (board(x, y) == Mark.X)
-            {
-                return Mark.X;
-            }
+                return Mark.None;//return none
+    }
+            else if (board(x, y) == Mark.O)//checks if O
+    {
+                return Mark.O;//return O
+    }
+            else if (board(x, y) == Mark.X)//checks if X
+    {
+                return Mark.X;//return X
+    }
             else { 
                 //error
             }
@@ -45,15 +43,15 @@ namespace comp110_worksheet_6
 		// If the specified square is currently empty, fill it with mark and return true.
 		// If the square is not empty, leave it as-is and return False.
 		public bool SetSquare(int x, int y, Mark mark){
-            if (board(x, y) == mark.None)
+            if (board(x, y) == mark.None)//checks if tile is empty
             {
-                board(x, y) == mark
-                return true;
+                board(x, y) == mark//marks the board with the given input
+                return true;//returns true
             }
-            else if (board(x, y) == mark.O || board(x, y) == mark.X)
-            {
-                return false;
-            }
+            else if (board(x, y) == mark.O || board(x, y) == mark.X)//checks if tile has X or O
+    {
+                return false;//returns false
+    }
             else
             {
                 //error
@@ -62,17 +60,44 @@ namespace comp110_worksheet_6
 
 		// If there are still empty squares on the board, return false.
 		// If there are no empty squares, return true.
-		public bool IsBoardFull()
-		{
-			throw new NotImplementedException("TODO: implement this function and then remove this exception");
+		public bool IsBoardFull() {//loops the same quare 9 times does not go through all combinations
+            public int square = 0;//this is used to check how many squares of the board is full
+            for (int i=0; i < 8; i++) {//loops 9 times to check the 3x3 board
+                if (board(x, y) == mark.X || board(x, y) == mark.O){//checks if tile has X or O
+                    square++//increase square if the tile has been used
+                }
+            }
+            if (square = 9) {//checks if all 9 square have been fulled 
+                return true;//returns true
+            }
+            else {
+                return false;//returns false
+            }
 		}
 
 		// If a player has three in a row, return Mark.O or Mark.X depending on which player.
 		// Otherwise, return Mark.None.
-		public Mark GetWinner()
-		{
-			throw new NotImplementedException("TODO: implement this function and then remove this exception");
-		}
+		public Mark GetWinner(){//sorry for the brute force, baised on top left
+            if (board(0, 0) == Mark.X && board(1, 0) == Mark.X && board(2, 0) == Mark.X) { return Mark.X; }//across 1 for X 
+            else if (board(0, 1) == Mark.X && board(1, 1) == Mark.X && board(2, 1) == Mark.X) { return Mark.X; }//across 2 for X
+            else if (board(0, 2) == Mark.X && board(1, 2) == Mark.X && board(2, 2) == Mark.X) { return Mark.X; }//across 3 for X
+            else if (board(0, 0) == Mark.X && board(0, 1) == Mark.X && board(0, 2) == Mark.X) { return Mark.X; }//up and down 1 for X
+            else if (board(0, 1) == Mark.X && board(1, 1) == Mark.X && board(2, 1) == Mark.X) { return Mark.X; }//up and down 2 for X
+            else if (board(0, 2) == Mark.X && board(1, 2) == Mark.X && board(2, 2) == Mark.X) { return Mark.X; }//up and down 3 for X
+            else if (board(0, 2) == Mark.X && board(1, 1) == Mark.X && board(2, 0) == Mark.X) { return Mark.X; }//diagonal 1 for X
+            else if (board(0, 0) == Mark.X && board(1, 1) == Mark.X && board(2, 2) == Mark.X) { return Mark.X; }//diagonal 2 for X
+
+            else if (board(0, 0) == Mark.O && board(1, 0) == Mark.O && board(2, 0) == Mark.O) { return Mark.O; }//across 1 for O 
+            else if (board(0, 1) == Mark.O && board(1, 1) == Mark.O && board(2, 1) == Mark.O) { return Mark.O; }//across 2 for O
+            else if (board(0, 2) == Mark.O && board(1, 2) == Mark.O && board(2, 2) == Mark.O) { return Mark.O; }//across 3 for O
+            else if (board(0, 0) == Mark.O && board(0, 1) == Mark.O && board(0, 2) == Mark.O) { return Mark.O; }//up and down 1 for O
+            else if (board(0, 1) == Mark.O && board(1, 1) == Mark.O && board(2, 1) == Mark.O) { return Mark.O; }//up and down 2 for O
+            else if (board(0, 2) == Mark.O && board(1, 2) == Mark.O && board(2, 2) == Mark.O) { return Mark.O; }//up and down 3 for O
+            else if (board(0, 2) == Mark.O && board(1, 1) == Mark.O && board(2, 0) == Mark.O) { return Mark.O; }//diagonal 1 for O
+            else if (board(0, 0) == Mark.O && board(1, 1) == Mark.O && board(2, 2) == Mark.O) { return Mark.O; }//diagonal 2 for O
+
+    else { return Mark.None; }
+}
 
 		// Display the current board state in the terminal. You should only need to edit this if you are attempting the stretch goal.
 		public void PrintBoard()
